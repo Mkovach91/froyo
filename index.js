dc7329a4d7cb47841d040fc01bd36e694628b8e8
matt k <matt.kovach91@gmail.com>
2024-10-15 10:59:04 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Prompt the user to enter a list of flavors
+// Use prompt to capture user input
+
+// Split the user input into an array of flavors
+// Use split method to separate the string by commas
+
+// create an empty object to store the count of each flavor
+
+// Loop through the array of flavors
+  // Check if the flavor already exists in the object
+    // If it exists, increase the count by 1
+    // If it doesnt exist, add it to the object with a count of 1
+
+// Log the result to the browser console
+
+// Test with "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+// Result: { vanilla: 3, strawberry: 1, coffee: 2 }
