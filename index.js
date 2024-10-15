48192e5a38cd323b6ac70748acaba3667e34290b
matt k <matt.kovach91@gmail.com>
2024-10-15 11:35:03 -0500

    finished

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-// Prompt the user to enter a list of flavors
-// Use prompt to capture user input
+// prompt the user to enter a list of flavors
+// use prompt to capture user input
+let userInput = prompt (`Please enter a list of froyo flavors seperated by a comma` , `vanilla,vanilla,vanilla,strawberry,coffee,coffee`);
 
-// Split the user input into an array of flavors
-// Use split method to separate the string by commas
+// split the user input into an array of flavors
+// use split method to separate the string by commas
+let flavorList = userInput.split (`,`);
 
 // create an empty object to store the count of each flavor
+let flavorCount = {};
 
-// Loop through the array of flavors
-  // Check if the flavor already exists in the object
-    // If it exists, increase the count by 1
-    // If it doesnt exist, add it to the object with a count of 1
+// loop through the array of flavors
+for(let i = 0; i < flavorList.length; i ++) {
+  let flavor = flavorList[i]
+  // check if the flavor already exists in the object
+  if (flavorCount[flavor]){
+    // if it exists increase the count by 1
+    flavorCount[flavor] ++;
+  }
+  // if it doesnt exist add it to the object with a count of 1
+  else {
+    flavorCount[flavor] = 1
+  }
+}
+// log the result to the browser console
+console.log (flavorCount);
 
-// Log the result to the browser console
-
-// Test with "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
-// Result: { vanilla: 3, strawberry: 1, coffee: 2 }
+// test with "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+// result: { vanilla: 3, strawberry: 1, coffee: 2 }
